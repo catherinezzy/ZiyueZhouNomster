e0bbe43e104482b4d9cd054563ed44403ad3b466
Ziyue <catherine000000@gmail.com>
2016-02-05 05:13:50 +0000

    fix common infigurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Nomster::Application.configure do
-  config.action_mailer.default_url_options = { host: 'nomster-ziyue-zhou.herokuapp.com' } Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'nomster-ziyue-zhou.herokuapp.com' } 
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
